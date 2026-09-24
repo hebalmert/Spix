beb7e0e933a8b7f488fdebe27e669b9b23febcdb
Hebert Merchan <merchanhebert@gmail.com>
2026-09-23 21:16:53 -0400

    un amigracion

---
+++ Spix.AppBacken/Migrations/20260924011611_IndiceFacturaCliente.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spix.AppBacken.Migrations
+{
+    /// <inheritdoc />
+    public partial class IndiceFacturaCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Sells_CorporationId_ClientId_DateSell",
+                table: "Sells",
+                columns: new[] { "CorporationId", "ClientId", "DateSell" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sells_CorporationId_ClientId_DateSell",
+                table: "Sells");
+        }
+    }
+}
